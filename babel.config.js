02ade7b5c4ca09f5142f2809301816c4e320d787
Justin Kimbrell <jkimbrell@actengage.com>
2020-10-09 13:11:51 -0600

    pre-release commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     presets: [
-        '@vue/cli-plugin-babel/preset'
+        ['@vue/cli-plugin-babel/preset', {
+            browsers: [
+                "> 1%",
+                "last 2 versions",
+                "not dead"
+            ]
+        }]
     ]
 };
